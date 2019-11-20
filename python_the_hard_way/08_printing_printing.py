343088b3009aa143638f064e9ad60f854899e9b5
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 03:44:34 +1000

    08_printing_printing

--- python_the_hard_way/08_printing_printing.py
+++ python_the_hard_way/08_printing_printing.py
@@ -1,13 +1,14 @@
-#_*_coding:utf-8_*_
-formatter = "%r%r%r%r"
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+formatter = '%r%r%r%r'
 
 print formatter % (1, 2, 3, 4)
-print formatter % ("one", "two", "three", "four")
+print formatter % ('one', 'two', 'three', 'four')
 print formatter % (True, False, False, True)
 print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (
-        "I had this thing.",
-        "That you could type up right.",
-        "But it didn't sing.",
-        "So I said goodnight."
-        )
+print formatter % ('I had this thing.', 
+                   'That you could type up right.',
+                   "But it didn't sing", 
+                   'So I said goodnight.'
+                   )
