0a31b96dc6a6546215b5d41bd207e5db60823d7a
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 04:27:39 +1000

    prompting_and_passing

--- python_the_hard_way/14_prompting_and_passing.py
+++ python_the_hard_way/14_prompting_and_passing.py
@@ -1,22 +1,31 @@
-#_*_coding:utf-8_*_
-from sys import argv
+#_ * _coding: utf - 8 _ * _
+from sys
+import argv
 
 script, user_name = argv
 prompt = '> '
 
-print "Hi {0}, I'm the {1} script.". format(user_name , script)
-print "I'd like to ask you a few questions."
-print "Do you like me {0}". format(user_name)
+print ("Hi {0}, I'm the {1} script.".format(user_name, script))
+print ("I'd like to ask you a few questions.")
+print ("Do you like me {0}".format(user_name))
 likes = input(prompt)
 
-print "Where do you live {0}?". format(user_name)
+print ("Where do you live {0}?".format(user_name))
 lives = input(prompt)
 
-print "What kind of computer do you have?"
+print ("What kind of computer do you have?")
 computer = input(prompt)
 
-print """
-Alright, so you said {0} about liking me.
-You live in {1}. Not sure where that is.
-And you have a {2} computer. Nice.
-""" . format(likes, lives, computer)
+print ("""
+    Alright, so you said {
+        0
+    }
+    about liking me.
+    You live in {
+        1
+    }.Not sure where that is.
+    And you have a {
+        2
+    }
+    computer.Nice.
+""" . format(likes, lives, computer))
