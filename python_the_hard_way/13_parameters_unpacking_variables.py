e42d349e4792fa98dd7cb5ccc8e75a3c6ab5f75b
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 04:15:08 +1000

    /usr/bin/python3 /home/haraka/python_learning/python_the_hard_way/13_parameters_unpacking_variables.py

--- python_the_hard_way/13_parameters_unpacking_variables.py
+++ python_the_hard_way/13_parameters_unpacking_variables.py
@@ -1,28 +1,20 @@
-#_*_coding:utf-8_*_
-#script - другое название программы с раcширением .py
-#argv(argument variable) - список аргументов(параметров) 
-#   командной строки, передаваемых сценарию python (name_of_prog.py)
-#   др.сл. программе python
+#
+_ * _coding: utf - 8 _ * _# script - другое название программы с раcширением.py# argv(argument variable) - список аргументов(параметров)# командной строки, передаваемых сценарию python(name_of_prog.py)# др.сл.программе python
 
-from sys import argv
+from sys
+import argv
 
-#sys - это модуль(др. название - библиотека), содержит переменные
-#   и функции, имеющие отношение к интерпретатору и его окружению.
+# sys - это модуль(др.название - библиотека), содержит переменные# и функции, имеющие отношение к интерпретатору и его окружению.
 
 script, first, second, third = argv
 
-fourth = "enter your value: {0} ". format(input())
+fourth = "enter your value: {0} ".format(input())
 
-print "The script is called:", script       #argv[0]
-#The argv[0] - is the name of script
+print ("The script is called:"), script# argv[0]# The argv[0] - is the name of script
 
-print "Your first variable is:", first      #argv[1]
-print "Your second variable is:", second    #argv[2]
-print "Your third variable is:", third      #argv[3]
-print "Your fourth variable is: ", fourth   #out.val.of inp.function
+print ("Your first variable is:"), first# argv[1]
+print ("Your second variable is:"), second# argv[2]
+print "Your third variable is:", third# argv[3]
+print "Your fourth variable is: ", fourth# out.val.of inp.function
 
-#!!!ADD name after ran the .py file
-#command line:
-#python 13_parameters_... . py aleksandr
-#or
-#python 13_parameters_... .py 1st 2nd 3rd
+#!!!ADD name after ran the.py file# command line: #python 13 _parameters_....py aleksandr# or# python 13 _parameters_....py 1 st 2 nd 3 rd
