bb7358c0f89f929eda65b37a622a0d9b757a29b8
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 03:44:34 +1000

    07_more_printing

--- python_the_hard_way/07_more_printing.py
+++ python_the_hard_way/07_more_printing.py
@@ -1,32 +1,44 @@
-#_coding:utf-8_*_
-#вывести строку на экран
-print "Mary had a little lamb."
-#вывести строку на экран и подставить др.строку внутри данной строки
-print "Its fleece was white as %s." % 'snow'
-#вывести строку на экран
-print "And everywhere that Mary went."
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-#Эта строка выводится на экран десять раз в линию
-#..........
-print "." * 10 #what'd that do?
+# вывести строку на экран
 
-#инициализировать переменные и присвоить им строковые значения
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
+print 'Mary had a little lamb.'
 
-#watch that comma at the end. try removin it to see what happens
-#вывод на экран результат конкатенации строк
-#(,) - служит пробелом при выводе значений переменных на экран
+# вывести строку на экран и подставить др.строку внутри данной строки
+
+print 'Its fleece was white as %s.' % 'snow'
+
+# вывести строку на экран
+
+print 'And everywhere that Mary went.'
+
+# Эта строка выводится на экран десять раз в линию
+# ..........
+
+print '.' * 10  # what'd that do?
+
+# инициализировать переменные и присвоить им строковые значения
+
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# watch that comma at the end. try removin it to see what happens
+# вывод на экран результат конкатенации строк
+# (,) - служит пробелом при выводе значений переменных на экран
+
 print end1 + end2 + end3 + end4 + end5 + end6,
-#вывести на экран результат конкатенации строк
+
+# вывести на экран результат конкатенации строк
+
 print end7 + end8 + end9 + end10 + end11 + end12
