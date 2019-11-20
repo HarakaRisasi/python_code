97df50d3fd3c7c5c7ffc20528b86a34c3da8aa7e
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 04:55:02 +1000

    func_can_return_something

--- python_the_hard_way/21_function_can_return_something.py
+++ python_the_hard_way/21_function_can_return_something.py
@@ -1,32 +1,32 @@
 #_*_coding:utf-8_*_
 def add(a, b):
-    print "ADDING {0} + {1}". format(a, b)
+    print ("ADDING {0} + {1}". format(a, b))
     return a + b
 
 def substract(a, b):
-    print "SUBSTRACTING {0} - {1}". format(a, b)
+    print ("SUBSTRACTING {0} - {1}". format(a, b))
     return a - b
 
 def multiply(a, b):
-    print "MULTIPLYING {0} * {1}". format(a, b)
+    print ("MULTIPLYING {0} * {1}". format(a, b))
     return a * b
 
 def devide(a, b):
-    print "DEVIDING {0} / {1}". format(a, b)
+    print( "DEVIDING {0} / {1}". format(a, b))
     return a / b
 
-print "Let's do some math with just function!"
+print ("Let's do some math with just function!")
 
 age = add(30 , 5)
 height = substract(78, 4)
 weight = multiply(90, 2)
 iq = devide(100, 2)
 
-print "Age: {0}\nHeight: {1}\nWeight: {2}\nIQ: {3}". format(age, height, weight,iq)
+print ("Age: {0}\nHeight: {1}\nWeight: {2}\nIQ: {3}". format(age, height, weight,iq))
 
 #A puzzle for the extra credit, type it in anyway.
-print "Here is a puzzle."
+print ("Here is a puzzle.")
 
 what = add(age, substract(height, multiply(weight, devide(iq, 2))))
 
-print "That becomes: ", what, "Can you do it by hand?"
+print ("That becomes: ", what, "Can you do it by hand?")
