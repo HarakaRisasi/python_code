18cba8965a794b1a6e3b7dc0d31edcad546dc217
HarakaRisasi <harakarisasi@gmail.com>
2019-11-21 04:00:50 +1000

    11_quiz_asking_quastion

--- python_the_hard_way/11_asking_quastion.py
+++ python_the_hard_way/11_asking_quastion.py
@@ -3,10 +3,10 @@
 
 # raw_input - была переименована в (input) с python 3.0
 
-print 'How old are you?',
+print ('How old are you?'),
 age = input()
-print 'How tall are you?',
-height = raw_input()
-print 'How much do you weight?',
-weight = raw_input()
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print ('How tall are you?'),
+height = input()
+print ('How much do you weight?'),
+weight = input()
+print ("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
