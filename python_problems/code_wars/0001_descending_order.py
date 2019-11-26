d295a88d3674657affeec365a2b3276e2541c744
HarakaRisasi <harakarisasi@gmail.com>
2019-11-27 02:30:46 +1000

    sort_number_in_descending_order

---
+++ python_problems/code_wars/0001_descending_order.py
@@ -0,0 +1,32 @@
+# Sort numbers in descending order
+def Descending_Order(num):    
+    return int(''.join(sorted(str(num))[::-1]))
+
+print(Descending_Order(1021)) #=> 2110
+
+#Discription:
+#----------------------------------
+# Time: 767ms
+#----------------------------------
+# steps:
+# 0 input num >> 1021
+# 1 str(num) >> '1021' 
+# 2 sorted(str(num)) >> ['0', '1', '1', '2']
+# 3 [::-1] or reverse(slice list) >> ['2', '1', '1', '0']
+# 4 join list to string >> '2110'
+# 5 convert to int >> 2110
+#----------------------------------   
+#1
+# Another solution
+# return int(''.join(sorted(str(num), reverse=1)))
+
+#2
+# def Descending_Order(num):
+# lst = (sorted(list(map(int, str(num))))[::-1])
+# return int("".join(map(str, lst)))
+#----------------------------------
+# type check:
+# type(str(num)) >> <class 'str'>
+    
+# type(int(str(num))) >> <class 'int'>
+#----------------------------------
