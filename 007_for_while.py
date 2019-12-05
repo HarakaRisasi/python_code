7fc162cf849d8880a865178bcf45738050506970
HarakaRisasi <harakarisasi@gmail.com>
2019-12-06 02:31:43 +1000

    ex.007_for_while

---
+++ 007_for_while.py
@@ -0,0 +1,14 @@
+animals = ['a', 'b', 'c']
+for index in range(len(animals)):
+    print(index, animals[index])
+    #>> 
+    # 0 a
+    # 1 b
+    # 2 c
+
+for letter in 1, 3, 4:
+    print(letter)
+    #>> 
+    # 1
+    # 3
+    # 4
