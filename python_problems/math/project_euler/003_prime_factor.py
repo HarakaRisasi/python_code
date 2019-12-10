54dd9f40d8e9c23bf53762f02625cb04c14be32e
HarakaRisasi <harakarisasi@gmail.com>
2019-12-11 03:31:02 +1000

    ex.003_find_prime_factor_of_600851475143

--- python_problems/math/project_euler/003_prime_factor.py
+++ python_problems/math/project_euler/003_prime_factor.py
@@ -1,9 +1,33 @@
-# n = int(input())
-n = 6008513
-div = []
+# What is the largest prime factor of the number 600851475143 ?
 
-for i in range(2, n):
-    if n % i == 0:
-        div.append(i)
+import subprocess
+subprocess.call("clear")
 
-print(div)
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i < n:
+        if n % i:
+            i += 1
+        else:
+            n //= i # Floor-Divide and Assign(//=)
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def largest_prime_factor(n):
+    i = 2
+    while i < n:
+        if n % i:
+            i += 1
+        else:
+            n //= i # Floor-Divide and Assign(//=)
+    return n
+
+
+print(prime_factors(600851475143))
+print(largest_prime_factor(600851475143))
+
+print(prime_factors(13195))
+print(largest_prime_factor(13195))
