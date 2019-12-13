0e211bed38ccc6037207b66ed040df37ac46b188
HarakaRisasi <harakarisasi@gmail.com>
2019-12-14 05:06:26 +1000

    010_2_modify_method_exclaim()_for_ferrari_class

--- 010_2_inheritance.py
+++ 010_2_inheritance.py
@@ -41,3 +41,18 @@ give_me_a_ferrari = Ferrari()
 give_me_a_car.exclaim() #>> I'am a Car!
 give_me_a_ferrari.exclaim() #>> I'am a Car!
 # класс Ferrari унаследовал метод exclaim() класса Car
+
+# modify_method_exclaim()_for_Ferrari_class
+class Car():
+    def exclaim(self):
+        print('I\'m a car!')
+
+class Ferrari(Car):
+    def exclaim(self):
+        print('I\'m a car! But more, i\'m a Ferrari.')
+
+give_me_a_car = Car()
+give_me_a_car.exclaim() #>> I'm a car!
+
+give_me_a_ferrari = Ferrari() #>> I'm a car! But more, i'm a Ferrari.
+give_me_a_ferrari.exclaim()
