64f37c07fc3dc02ae37ac73ddf2d36b1a8099116
Haraka Risasi <harakarisasi@gmail.com>
2020-01-08 19:25:55 +0700

    scraping dynamic web with Py and selenium

---
+++ scrap/tests/test_2.py
@@ -0,0 +1,6 @@
+# Scraping Dynamic Web Pages with Python and Selenium
+# Начало работы с Selenium
+#
+# Создается экземпляр Chromedriver, указывающий отправную точку на папку, в которую я его установил.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
