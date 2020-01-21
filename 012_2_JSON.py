92a3d600e1556b8a0f9f802e7dd19d85ffc18cb7
Haraka Risasi <harakarisasi@gmail.com>
2020-01-21 23:24:02 +0700

    maked json file which stored data (input from keyboard)

--- 012_2_JSON.py
+++ 012_2_JSON.py
@@ -1,7 +1,9 @@
 import json
 
+#input data from keyboard
 username = input("input your name: ")
 
+# maked file like a json(write data into JSON file)
 filename = './json_ex/user.json'
 with open(filename, 'w') as fl:
     json.dump(username, fl)
