c1c8499b3d68dae25d91deb9b270d52497de7f90
Haraka Risasi <harakarisasi@gmail.com>
2020-01-22 00:26:42 +0700

    update code: get program with two functions

--- 012_3_JSON_searching.py
+++ 012_3_JSON_searching.py
@@ -3,21 +3,37 @@ import json
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+def get_username():
+    """Get username"""
+    # we have a file user.json
+    filename = './json_ex/user.json'
+    # program try opened file user.json into path "./json_ex/user.json" and use variable 'filename'
+    try: 
+        with open(filename) as f:
+            # write in var 'user' the data was stored into this file 'user.json'
+            user = json.load(f)    
+    # If the program does not find the file "filename" then return NONE
+    except FileNotFoundError:
+        print("Error file was not found")
+        return None
+        
+    else:
+        return user
 
-filename = './json_ex/user.json'
+def greet_user():
+    """Greeting user"""
+    # make var "username" and var get data which was return from function 'get_username'
+    username = get_username()
+    # if such data was existed then print Greeating
+    if username:
+        print(f'Greeting {username}!')  
+    # else then input new data from keyboard and make new file.json and write into file this new data
+    else:   
+        username = input("input your name: ")
+        filename = './json_ex/user.json'
+        with open(filename, 'w', encoding='utf-8') as fl:
+            json.dump(username, fl, ensure_ascii=False)
+            # print message
+            print(f'We are save your name like a {username}!')
 
-# program opened file "filename" into "./json_ex/user.json"
-try: 
-    with open(filename) as fl:
-        user = json.load(fl)
-
-# If the program does not find the file "filename" then it creates a new file
-except:
-    user = input("input your name: ")
-    with open(filename, 'w', encoding='utf-8') as fl:
-        json.dump(user, fl, ensure_ascii=False)
-        print(f'We are save your name like a {user}!')
-
-# If the program was able to open the file then print message
-else:
-    print(f"Greating {user}!")
+greet_user()
