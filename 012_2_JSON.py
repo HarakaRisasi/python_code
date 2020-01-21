a1c2f7cc5fdaf1897fde18abb503d17aa54c58a1
Haraka Risasi <harakarisasi@gmail.com>
2020-01-21 23:30:19 +0700

    add understanding cyrillic characters

--- 012_2_JSON.py
+++ 012_2_JSON.py
@@ -5,6 +5,9 @@ username = input("input your name: ")
 
 # maked file like a json(write data into JSON file)
 filename = './json_ex/user.json'
-with open(filename, 'w') as fl:
-    json.dump(username, fl)
+# For JSON understanding cyrillic characters(Symbols)
+# encoding="utf-8"
+# ensure_ascii=False
+with open(filename, 'w', encoding="utf-8") as fl: 
+    json.dump(username, fl, ensure_ascii=False)
     print(f'We are save your name like a {username}!')
