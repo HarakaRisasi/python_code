7b824d9d717fa2f90092703e30be9957fd15170b
Haraka Risasi <harakarisasi@gmail.com>
2020-02-04 07:12:27 +0700

    update code

--- test_python/001_dictionary_part_1.py
+++ test_python/001_dictionary_part_1.py
@@ -1,1 +1,16 @@
+#Import library
+import json
+
 # Load the data, and just check if data loaded correctly.
+# Try typing "type(data)" in terminal after executig firs two line of this snippet
+data = json.load(open("data.json"))
+
+# Function for retrieving definition
+def retrive_definition(word):
+    return data[word]
+
+# Input from user
+word_user = input("Enter a word: ")
+
+# Retrive the definition using function and print the result
+print(retrive_definition(word_user))
